26e6d66a5579074d9a0c28bf938fd595907f9280
Luisedu012 <luiseduardorm012@gmail.com>
2025-11-27 15:30:02 -0300

    fix: corrigir docker

--- docker/scripts/healthcheck.sh
+++ docker/scripts/healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Healthcheck simples
-#wget --no-verbose --tries=1 --spider http://localhost:8000/health || exit 1
+wget --no-verbose --tries=1 --spider http://localhost:8000/health || exit 1
